b7d4880e642642a40f63c61c0afd9406032e4c27
Luiz Fernando Megale Paiva <luiz_paiva@gec.inatel.br>
2023-07-24 21:26:07 -0300

    finalizando aula 3

---
+++ sorteador-de-amigo-secreto/src/paginas/Configuracao.tsx
@@ -0,0 +1,15 @@
+import Formulario from "../componentes/Formulario";
+import ListaParticipantes from "../componentes/ListaParticipantes";
+import Rodape from "../componentes/Rodape/Rodape";
+
+const Configuracao = () => {
+  return (
+    <>
+      <Formulario />
+      <ListaParticipantes />
+      <Rodape />
+    </>
+  );
+};
+
+export default Configuracao;
